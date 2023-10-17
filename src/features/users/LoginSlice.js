8bf643bb587877885649911fc4b4ef8395d2c2b3
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-10-17 11:01:45 -0300

    add login slice and func

--- src/features/users/LoginSlice.js
+++ src/features/users/LoginSlice.js
@@ -1,1 +1,48 @@
+import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
+export const login = createAsyncThunk("login/login", async (data) => {
+  try {
+    const response = await axios.post(
+      "https://backendnodearticles-nppixgnv.b4a.run/api/login",
+      data
+    );
+    if (response.status === 200) {
+      localStorage.setItem("token", response.data.token);
+    }
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+});
+
+const loginSlice = createSlice({
+  name: "login",
+  initialState: {
+    isLoading: false,
+    token: "",
+    isError: false,
+  },
+    reducers: {
+    logout: (state, action) => {
+      localStorage.removeItem("token");
+      state.token = "";
+
+    },
+    },
+  extraReducers: (builder) => {
+    builder.addCase(login.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.token = action.payload.token;
+    });
+    builder.addCase(login.rejected, (state, action) => {
+      state.isError = true;
+    });
+  },
+});
+export const { logout} = loginSlice.actions
+
+export default loginSlice.reducer;

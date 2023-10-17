7a0a9814ae61eab49fee5153aca93510987b5f67
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-10-17 11:01:15 -0300

    add welcomepage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import  { Routes , Route} from 'react-router-dom';
 import Home from './components/Home';
 import Login from './features/users/Login';
+import WelcomePage from './components/WelcomePage';
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
           <Home />
         } />
         <Route path="/login" element={<Login />}
+        />
+        <Route path="/welcome" element={<WelcomePage />}
         />
 
       </Routes>
